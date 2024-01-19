607c0b65dfbd4333eb70b403f306bff56df1d117
Toastie <toastie@toastiet0ast.com>
2024-01-19 23:55:32 +1300

    Updated patchnotes site to use astro

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const releases = defineCollection({
+	// Type-check frontmatter using a schema
+	schema:
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			versionNumber: z.string(),
+			// Transform string to Date object
+			date: z.date({ coerce: true }),
+		}),
+});
+
+export const collections = { releases };
